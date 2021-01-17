b93f6d9980424f300e661ea56ac9734dcc1a8431
Miguel Sanz <msanz1610@gmail.com>
2021-01-17 10:37:33 -0300

    feat: actualizacion layout dashboard

--- src/@system/services/api.service.ts
+++ src/@system/services/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gender, Species } from 'app/models/charters.model';
 
 import { map } from 'rxjs/operators';
 
@@ -10,11 +11,28 @@ export class ApiService {
   url: string;
 
   constructor(private http: HttpClient) {
-    this.url = 'https://rickandmortyapi.com/api/character';
+    this.url = 'https://rickandmortyapi.com/api/';
   }
 
-  getCharters() {
-    return this.http.get(this.url).pipe(
+  getCharters(route: string) {
+    return this.http.get(`${this.url}/${route}`).pipe(
+      map((resp: any) => {
+        return resp;
+      })
+    );
+  }
+
+  getCharter(route: string, id: string) {
+    return this.http.get(`${this.url}/${route}/${id}`).pipe(
+      map((resp: any) => {
+        return resp;
+      })
+    );
+  }
+
+  filterCharter(gender: Gender, species: Species) {
+    const url = `${this.url}/character/?gender=${gender}&species=${species}`;
+    return this.http.get(url).pipe(
       map((resp: any) => {
         return resp;
       })
--- src/app/main/public/pages/home/home.component.ts
+++ src/app/main/public/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '@system/services/api.service';
+import { Result } from 'app/models/charters.model';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,46 @@ import { ApiService } from '@system/services/api.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  characters: Result[] = [];
+  info: any;
 
-  ngOnInit(): void {}
+  constructor(private _apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.getCharters();
+  }
+
+  getCharters() {
+    this._apiService.getCharters('character').subscribe((resp: any) => {
+      this.characters = resp.results;
+      this.info = resp.info;
+    });
+  }
+
+  next() {
+    const route = this.info.next.replace(
+      'https://rickandmortyapi.com/api/',
+      ''
+    );
+    this._apiService.getCharters(route).subscribe((resp: any) => {
+      this.characters = resp.results;
+      this.info = resp.info;
+      console.log(this.characters);
+    });
+  }
+
+  prev() {
+    const route = this.info.prev.replace(
+      'https://rickandmortyapi.com/api/',
+      ''
+    );
+    if (this.info.prev == null) {
+      return;
+    }
+    this._apiService.getCharters(route).subscribe((resp: any) => {
+      this.characters = resp.results;
+      this.info = resp.info;
+      console.log(this.characters);
+    });
+  }
 }
